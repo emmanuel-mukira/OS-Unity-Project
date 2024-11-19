a24d2938e200705391b55d9803b48fbeed0261a0
Nooman29 <nooman.rafiq@strathmore.edu>
2024-11-19 20:04:38 +0300

    peterson 2

--- Assets/Scripts/Patient.cs
+++ Assets/Scripts/Patient.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class Patient : MonoBehaviour
 {
-    public float arrivalTime;
-    public float serviceTime;
-    public float waitingTime;
-    public float turnaroundTime;
-    public bool isServed = false;
+    public float arrivalTime; // Time when the patient arrives
+    public float serviceTime; // Time required to be treated
+    public float remainingServiceTime; // Time left for treatment (used in SRTF)
+    public float waitingTime; // Time the patient has been waiting
+    public float turnaroundTime; // Time from arrival to completion of treatment
+
+     private void Start()
+    {
+        // Initialize remainingServiceTime with the initial serviceTime
+        remainingServiceTime = serviceTime;
+    }
+
+    public Animator animator; // Reference to the Animator for animations
 }
